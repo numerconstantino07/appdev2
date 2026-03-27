b3a4ca3eaf2c3d4ddbc84ef2c57fb45dcd2040a7
Numer Constantino <numerconstantino07@gmail.com>
2026-03-27 16:49:22 +0800

    Done playing and updating the 01-basic.ts file

--- appdev2-midterm-exam/App.js
+++ appdev2-midterm-exam/App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
-// Import your screens
 import WelcomeScreen from "./src/screens/WelcomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import SignupScreen from "./src/screens/SignupScreen";
-import TodoScreen from "./src/screens/TodoScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Change the screen here when testing */}
       <WelcomeScreen />
-      {/* <LoginScreen /> */}
-      {/* <SignupScreen /> */}
-      {/* <TodoScreen /> */}
-      
       <StatusBar style="auto" />
     </View>
   );
